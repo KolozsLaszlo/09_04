5a5a74122101e06e184bd0d78b0a6db23f69ea15
kzs <Kzs@a.a>
2023-09-12 08:39:30 +0200

    card style

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const helloh1 = document.querySelector('#hello')
 
 helloh1.addEventListener('click', () => {
   let card = document.createElement('div')
+  card.classList.add('kartya')
   card.innerText = Math.floor(Math.random() * 6) + 1
   container.append(card)
 })
