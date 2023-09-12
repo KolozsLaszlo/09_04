75323d281c39614c45e37cf7a7c3da3d2e3aacf9
kzs <Kolozsvari@a.a>
2023-09-12 10:17:32 +0200

    dobokóckák hozzádva

--- script.js
+++ script.js
@@ -2,21 +2,32 @@ console.error('hiba')
 console.log('hello')
 const container = document.querySelector('#container')
 const helloh1 = document.querySelector('#hello')
-let cards = [] // Array to store 10 random numbers
+const diceImages = [
+  'dice-1.png', // Replace with actual image URLs
+  'dice-2.png',
+  'dice-3.png',
+  'dice-4.png',
+  'dice-5.png',
+  'dice-6.png',
+]
 
-// Generate 10 random numbers and store them in the 'cards' array
-for (let i = 0; i < 10; i++) {
-  cards.push(Math.floor(Math.random() * 6) + 1)
+const initialDiceCount = 10
+let diceCount = initialDiceCount
+
+function displayDice() {
+  let randomIndex = Math.floor(Math.random() * diceImages.length)
+  let diceImage = document.createElement('img')
+  diceImage.classList.add('kocka')
+  diceImage.src = diceImages[randomIndex]
+  container.appendChild(diceImage)
+  diceCount++
 }
 
-let currentIndex = 0 // Initialize index to show the first card
+// Display the initial 10 dice faces
+for (let i = 0; i < initialDiceCount; i++) {
+  displayDice()
+}
 
 helloh1.addEventListener('click', () => {
-  if (currentIndex < cards.length) {
-    let card = document.createElement('div')
-    card.classList.add('kartya')
-    card.innerText = cards[currentIndex]
-    container.append(card)
-    currentIndex++
-  }
+  displayDice()
 })
