541c316bab8bd0e44de70b4dbf36e72ae6bf9091
KolozsLaszlo <119128615+KolozsLaszlo@users.noreply.github.com>
2023-09-17 19:40:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-console.error('hiba')
-console.log('hello')
-const container = document.querySelector('#container')
-const helloh1 = document.querySelector('#hello')
-const diceImages = [
-  'dice-1.png', // Replace with actual image URLs
-  'dice-2.png',
-  'dice-3.png',
-  'dice-4.png',
-  'dice-5.png',
-  'dice-6.png',
-]
+console.error('hiba');
+console.log('hello');
+const container = document.querySelector('#container');
+const helloh1 = document.querySelector('#hello');
 
-const initialDiceCount = 10
-let diceCount = initialDiceCount
+const diceCharacters = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
 
-function displayDice() {
-  let randomIndex = Math.floor(Math.random() * diceImages.length)
-  let diceImage = document.createElement('img')
-  diceImage.classList.add('kocka')
-  diceImage.src = diceImages[randomIndex]
-  container.appendChild(diceImage)
-  diceCount++
+const initialDiceCount = 10;
+
+function displayRandomDice() {
+  container.innerHTML = ''; // Clear previous dice faces
+  const randomDiceSet = getRandomDiceSet(initialDiceCount);
+  randomDiceSet.forEach(diceNumber => {
+    let diceCharacter = document.createElement('div');
+    diceCharacter.classList.add('dice-face'); // Add the 'dice-face' class
+    diceCharacter.textContent = diceCharacters[diceNumber];
+    container.appendChild(diceCharacter);
+  });
+  container.style.display = 'flex'; // Display the dice faces in a single line
 }
 
-// Display the initial 10 dice faces
-for (let i = 0; i < initialDiceCount; i++) {
-  displayDice()
+function getRandomDiceSet(count) {
+  const randomDiceSet = [];
+  for (let i = 0; i < count; i++) {
+    const randomIndex = Math.floor(Math.random() * diceCharacters.length);
+    randomDiceSet.push(randomIndex);
+  }
+  return randomDiceSet;
 }
+
+// Display 10 random dice faces initially
+displayRandomDice();
 
 helloh1.addEventListener('click', () => {
-  displayDice()
-})
+  // Generate and display 10 new random dice faces
+  displayRandomDice();
+});
